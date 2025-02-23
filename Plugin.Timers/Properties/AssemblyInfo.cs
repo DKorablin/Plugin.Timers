d39b66c764d8d69b95b9201ecab3cdaff203de1d
DKorablin2 <danila.korablin@unity3d.com>
2025-02-24 01:34:13 +0300

    Unify plugins deployment

--- Plugin.Timers/Properties/AssemblyInfo.cs
+++ Plugin.Timers/Properties/AssemblyInfo.cs
@@ -1,7 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: ComVisible(false)]
 [assembly: Guid("69c79417-c168-434a-a597-4e224237a527")]
 [assembly: System.CLSCompliant(true)]
 
@@ -9,14 +8,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyMetadata("ProjectUrl", "https://dkorablin.ru/project/Default.aspx?File=119")]
 #else
 
-[assembly: AssemblyTitle("Plugin.Timers")]
 [assembly: AssemblyDescription("Timers scheduler service plugin")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Danila Korablin")]
-[assembly: AssemblyProduct("Plugin.Timers")]
 [assembly: AssemblyCopyright("Copyright © Danila Korablin 2018-2024")]
 #endif
